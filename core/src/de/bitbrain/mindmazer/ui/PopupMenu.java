b2e32a810fa2eacc73771beafd21000e52f01547
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2017-08-19 23:47:17 +0100

    Add toggle menu animation

--- core/src/de/bitbrain/mindmazer/ui/PopupMenu.java
+++ core/src/de/bitbrain/mindmazer/ui/PopupMenu.java
@@ -15,7 +15,14 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.Align;
 
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenEquations;
+import aurelienribon.tweenengine.TweenManager;
 import de.bitbrain.braingdx.assets.SharedAssetManager;
+import de.bitbrain.braingdx.tweens.ActorTween;
+import de.bitbrain.braingdx.tweens.SharedTweenManager;
 import de.bitbrain.mindmazer.Colors;
 
 public class PopupMenu extends Group {
@@ -26,14 +33,33 @@ public class PopupMenu extends Group {
 	
 	private ImageButton menuButton;
 	
+	private final TweenManager tweenManager = SharedTweenManager.getInstance();
+	
 	public PopupMenu() {
 		menuButton = new ImageButton(Styles.IMAGEBUTTON_POPUPMENU);
 		menuButton.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				super.clicked(event, x, y);
-				for (Table group : entries) {
-					group.setVisible(!group.isVisible());
+				for (int i = 0; i < entries.size(); ++i) {
+					final Table group = entries.get(i);
+					tweenManager.killTarget(group);
+					Tween tween = Tween.to(group, ActorTween.ALPHA, 1f).delay(i * 0.2f)
+					     .target(group.isVisible() ? 0f : 1f);
+					if (!group.isVisible()) {
+						group.setVisible(true);
+						tween.ease(TweenEquations.easeInCubic);
+					} else {
+						tween.ease(TweenEquations.easeOutCubic);
+						tween.setCallback(new TweenCallback() {
+							@Override
+							public void onEvent(int type, BaseTween<?> source) {
+								group.setVisible(false);
+							}
+						});
+						tween.setCallbackTriggers(TweenCallback.COMPLETE);
+					}
+					tween.start(tweenManager);
 				}
 			}
 		});
@@ -63,6 +89,7 @@ public class PopupMenu extends Group {
 		entry.add(iconButton).padLeft(PADDING);
 		addActor(entry);
 		entry.setVisible(false);
+		entry.getColor().a = 0f;
 		entry.setPosition(-description.getPrefWidth() - PADDING, PADDING * 1.5f + entries.size() * (iconButton.getPrefHeight() + PADDING));
 	}
 }
