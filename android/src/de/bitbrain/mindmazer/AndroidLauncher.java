c8c7ac275c1a5e52eb5d87ac86472a22630bd089
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2017-08-20 10:50:40 +0100

    Integrate GooglePlayServices API

--- android/src/de/bitbrain/mindmazer/AndroidLauncher.java
+++ android/src/de/bitbrain/mindmazer/AndroidLauncher.java
@@ -2,17 +2,29 @@ package de.bitbrain.mindmazer;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.example.games.basegameutils.GameHelper.GameHelperListener;
 
 import android.os.Bundle;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication implements GameHelperListener {
+	
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.numSamples = 2;
 		config.useCompass = false;
 		config.useWakelock = true;
 		initialize(new MindmazerGame(), config);
+	}
+
+	@Override
+	public void onSignInFailed() {
+		// TODO
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+		// TODO
 	}
 }
--- core/src/de/bitbrain/mindmazer/Config.java
+++ core/src/de/bitbrain/mindmazer/Config.java
@@ -5,12 +5,12 @@ public interface Config {
    String GAME_NAME = "mindmazer";
    String GAME_VERSION = "v0.8.1";
    
-   boolean DEBUG = true;
+   boolean DEBUG = false;
    int TILE_SIZE = 16;
    float BASE_ZOOM_DESKTOP = 0.2f;
    float BASE_ZOOM = 0.02f;
    int DEFAULT_LIFE = 3;
-   float MUSIC_VOLUME = 0.5f;
+   float MUSIC_VOLUME = 0.7f;
    int SWIPE_TOLERANCE = 8;
    int SEED_STRING_LENGTH = 24;
    
